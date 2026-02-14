78e855ca827ab875ed40ddac91d1357157c2ef1a
Giovanna Roberta <giiroberta18@gmail.com>
2026-02-13 23:25:41 -0300

    :sparkles: feat: implement authentication flow with Axios, services and AuthContext

--- src/pages/home/cadastro/Cadastro.tsx
+++ src/pages/home/cadastro/Cadastro.tsx
@@ -1,72 +1,169 @@
+import { useEffect, useState, type ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+import type { Usuario } from "../../../models/Usuario";
+import { cadastrarUsuario } from "../../../services/Service";
+
 function Cadastro() {
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [confirmarSenha, setConfirmarSenha] = useState<string>("");
+
+  const [usuario, setUsuario] = useState<Usuario>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+  });
+
+  function retornar() {
+    navigate("/login");
+  }
+
+  useEffect(() => {
+    if (usuario.id !== 0) {
+      retornar();
+    }
+  }, [usuario]);
+
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmarSenha(e.target.value);
+  }
+
+  async function cadastrarNovoUsuario(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (confirmarSenha === usuario.senha && usuario.senha.length >= 8) {
+      setIsLoading(true);
+
+      try {
+        await cadastrarUsuario("/usuarios/cadastrar", usuario, setUsuario);
+        alert("Cadastrou o usuário... paz");
+      } catch (error) {
+        alert("Erro ao cadastar o usuario");
+        console.log(error);
+      }
+    } else {
+      alert("Dados errados, confere isso ai.");
+      setUsuario({ ...usuario, senha: "" });
+      setConfirmarSenha("");
+    }
+    setIsLoading(false);
+  }
+
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
-        <div className="bg-[url('https://i.imgur.com/ZZFAmzo.jpg')] lg:block hidden bg-no-repeat w-full min-h-screen bg-cover bg-center"></div>
-        <form className="flex justify-center items-center flex-col w-2/3 gap">
+        <div className="bg-[url('https://ik.imagekit.io/2zvbvzaqt/google.jpg')] lg:block hidden bg-no-repeat w-full min-h-screen bg-contain bg-left"></div>
+
+        <form
+          onSubmit={cadastrarNovoUsuario}
+          className="flex justify-center items-center flex-col w-2/3 gap-3"
+        >
           <h2 className="text-slate-900 text-5xl">Cadastrar</h2>
+
           <div className="flex flex-col w-full">
             <label htmlFor="nome">Nome</label>
             <input
               type="text"
               id="nome"
               name="nome"
-              placeholder="Nome"
+              placeholder="Seu nome aqui..."
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.nome}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
+
           <div className="flex flex-col w-full">
-            <label htmlFor="usuario">Usuário</label>
+            <label htmlFor="usuario">Usuario</label>
             <input
               type="text"
               id="usuario"
               name="usuario"
-              placeholder="Usuario"
+              placeholder="Seu usuario aqui..."
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
+
           <div className="flex flex-col w-full">
             <label htmlFor="foto">Foto</label>
             <input
               type="text"
               id="foto"
               name="foto"
-              placeholder="Foto"
+              placeholder="Sua foto aqui..."
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.foto}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
+
           <div className="flex flex-col w-full">
             <label htmlFor="senha">Senha</label>
             <input
               type="password"
               id="senha"
               name="senha"
-              placeholder="Senha"
+              placeholder="Sua senha aqui..."
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
+
           <div className="flex flex-col w-full">
             <label htmlFor="confirmarSenha">Confirmar Senha</label>
             <input
               type="password"
               id="confirmarSenha"
               name="confirmarSenha"
-              placeholder="Confirmar Senha"
+              placeholder="Confirme a senha"
               className="border-2 border-slate-700 rounded p-2"
+              value={confirmarSenha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                handleConfirmarSenha(e)
+              }
             />
           </div>
+
           <div className="flex justify-around w-full gap-8">
             <button
               type="reset"
               className="rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2"
+              onClick={retornar}
             >
               Cancelar
             </button>
             <button
               type="submit"
-              className="rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2 flex justify-center"
+              className="rounded text-white flex justify-center bg-emerald-400 hover:bg-emerald-700 w-1/2 py-2"
             >
-              Cadastrar
+              {isLoading ? (
+                <ClipLoader color="#FFFFFF" size={12} />
+              ) : (
+                <span>Cadastrar</span>
+              )}
             </button>
           </div>
         </form>
