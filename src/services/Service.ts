32e61917f6fd63256e6654c516f3a345c425382e
Giovanna Roberta <giiroberta18@gmail.com>
2026-02-20 10:43:36 -0300

    :sparkles: feat: implement CRUD for Tema with components, styling and logic

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backenddothiagojs12.onrender.com", //endereco do meu backend
+  baseURL: "https://blog-pessoal-1zmn.onrender.com/", //endereco do meu backend
 });
 
 export const cadastrarUsuario = async (
@@ -17,3 +17,36 @@ export const login = async (url: string, dados: object, setDados: Function) => {
   const resposta = await api.post(url, dados);
   setDados(resposta.data);
 };
+
+export const buscar = async (
+  url: string,
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.get(url, header);
+  setDados(resposta.data);
+};
+
+export const cadastrar = async (
+  url: string,
+  dados: object,
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.post(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const atualizar = async (
+  url: string,
+  dados: object,
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.put(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header)
+}
